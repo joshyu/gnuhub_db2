3abc75ac66b2943eff2e0c94d5a0c9ef97af297f
gnuhub <gnuhub@gmail.com>
2012-12-27 14:55:24 +0800

    让用户仅能修正自己的数据库

--- fixDB.sh
+++ fixDB.sh
@@ -6,7 +6,7 @@ then
         exit 1
 fi
 ##################################################
-sudo chmod +x /home/${1}/sqllib/adm/*
+chmod +x /home/${1}/sqllib/adm/*
 ##################################################
 configTool.sh printAll -configPath /home/${1}/sqllib/db2tss/config
 configTool configureParams -configPath /home/${1}/sqllib/db2tss/config -installPath /home/${1}/sqllib/db2tss -logPath /home/${1}/sqllib/db2tss/log
